8dc72c6e7e21ddffef5a9edf4b6406b8f5b999dc
Vladimir Moravec <vmoravec@suse.com>
2015-11-04 00:01:13 +0100

    Small improvements in Cli

--- lib/pedro/cli.ex
+++ lib/pedro/cli.ex
@@ -1,6 +1,7 @@
 defmodule Pedro.Cli do
   @moduledoc """
   Handle command line parsing and dispatch functions
+  Modules nested in the Pedro.Cli namespace contain command implementation
   """
 
   def main(argv) do
@@ -38,11 +39,26 @@ defmodule Pedro.Cli do
     IO.puts "Pedro help: comes soon"
   end
 
-  def run_action switches, commands do
+  defp run_action switches, commands do
     [ command | values ] = commands
     [ module, fun ] = String.split(command, ".")
-    module_name = String.to_atom("Elixir.Pedro.Cli.#{String.capitalize(module)}")
-    Kernel.apply(module_name, String.to_atom(fun), [values])
+    try do
+      Kernel.apply(
+        String.to_atom("Elixir.Pedro.Cli.#{String.capitalize(module)}"),
+        String.to_atom(fun),
+        [resolve(switches, command, values)]
+      )
+    rescue
+      e in UndefinedFunctionError -> IO.puts("Command does not exist: #{inspect e}")
+    end
+  end
+
+  defp resolve switches, command, values do
+    switches |> resolve_command(command, values)
+  end
+
+  defp resolve_command switches, command, values do
+    [local: true, node: :bla, doma: :som]
   end
 
 end
--- lib/pedro/cli/node.ex
+++ lib/pedro/cli/node.ex
@@ -1,13 +1,13 @@
 defmodule Pedro.Cli.Node do
   alias Pedro.Runner
 
-  def list args do
+  def list options do
     case detect_node do
       {:local, name } -> Runner.local(name, :E, :b, [])
     end
   end
 
-  def detect_node do
+  defp detect_node do
     {:local, :os.getenv("PEDRO_SERVER") || "pedro-server"}
   end
 end
